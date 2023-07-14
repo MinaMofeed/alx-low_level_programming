ce4578d8ae7d624f803c0968570d815b9d31cef1
MinaMofid <gado.mina@gmail.com>
2023-07-14 13:10:49 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - A program that prints a line with the puts function
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
