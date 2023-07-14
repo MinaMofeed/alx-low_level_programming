08a488aeead97800f35529a3e3b84b37bc608115
MinaMofid <gado.mina@gmail.com>
2023-07-14 15:03:57 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - A program that prints a line with the puts function
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
