8caf7c7f79b821a58f0b889d232146a037df8dfa
MinaMofid <gado.mina@gmail.com>
2023-07-13 17:42:56 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - A program that prints a line with the puts function
+*Return: Always 1 (success)
+*/
+int main(void)
+{
+fput("nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+return (1);
+}
