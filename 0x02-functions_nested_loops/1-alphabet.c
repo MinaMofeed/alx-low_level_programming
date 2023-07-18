c4a1d17050edb4b69fac19f03b287e036220aeea
MinaMofid <gado.mina@gmail.com>
2023-07-18 07:20:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - A program that prints a line with the puts function
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
