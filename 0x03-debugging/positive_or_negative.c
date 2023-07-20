dc53970c91adcf6a47bcc50aeadffa02a75f0ec5
MinaMofid <gado.mina@gmail.com>
2023-07-20 09:46:32 -0700

    positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,13 +1,14 @@
-include "main.h"
+#include "main.h"
 
-void positive_or_negitive(int i)
+void positive_or_negative(int i)
 {
-	if (i > 0)
+	if (i > 0){
 		printf("%d is positive\n", i);
-	else if (i < 0)
-
+	}
+	else if (i < 0){
 		printf("%d is negitive\n", i);
-	else
-
+	}
+	else{
 		printf("%d is zero\n", i);
+	}
 }
